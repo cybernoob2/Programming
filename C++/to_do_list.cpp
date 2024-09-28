03d5f95b2fe2a730cb9ca491fd8c2b0f5d7a3a7c
cybernoob2 <ndiravic@gmail.com>
2024-09-28 14:20:05 +0300

    first commit

---
+++ C++/to_do_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is a to do list"<<endl;
+    
+    do
+    {
+        cout << ""
+    } while (/* condition */);
+    
+
+}
