2574da14bdf4cc51b2b916f8daccafd96f24ddfb
cybernoob2 <ndiravic@gmail.com>
2024-10-04 06:41:36 +0300

    Another commit

--- C++/Chat gpt/2d_array.cpp
+++ C++/Chat gpt/2d_array.cpp
@@ -25,7 +25,7 @@ int main() {
     }
 
     // Step 4: Display the 2D array (optional)
-    cout << "The 2D array is:" << endl;
+    cout << "The 2D array is:" << ---------------------------
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             cout << array[i][j] << " " ;
